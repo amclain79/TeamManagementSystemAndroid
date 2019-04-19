0f01a947663a76cf9dc2ab78984bef3bae21a94e
Aaron McLain <aaron.mclain@mavs.uta.edu>
2019-04-19 15:25:18 -0500

    Finished create team use case.

--- app/src/main/java/edu/uta/cse5324/team1/teammanagementsystemandroid/CreateTeamForm.java
+++ app/src/main/java/edu/uta/cse5324/team1/teammanagementsystemandroid/CreateTeamForm.java
@@ -1,8 +1,12 @@
 package edu.uta.cse5324.team1.teammanagementsystemandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import controller.CreateTeamController;
+import model.CreateTeamRequest;
 
 public class CreateTeamForm extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class CreateTeamForm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_team_form_layout);
+    }
+
+    public void createTeam(View view){
+        TextView teamNameTV = findViewById(R.id.teamName);
+        String teamName = teamNameTV.getText().toString();
+
+        CreateTeamRequest ctr = new CreateTeamRequest(
+                teamName, Main.email
+        );
+
+        try{
+            Main.createTeamController.createTeam(ctr);
+            Intent memberMenu = new Intent(this, MemberMenuGUI.class);
+            memberMenu.putExtra("previous", "CreateTeamForm");
+            startActivity(memberMenu);
+        }catch (CreateTeamController.InvalidTeamName e){
+            Informer.inform("Invalid Team Name.", findViewById(R.id.createProfileLayout));
+        }
     }
 }
--- app/src/main/java/edu/uta/cse5324/team1/teammanagementsystemandroid/MemberMenuGUI.java
+++ app/src/main/java/edu/uta/cse5324/team1/teammanagementsystemandroid/MemberMenuGUI.java
@@ -1,7 +1,6 @@
 package edu.uta.cse5324.team1.teammanagementsystemandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class MemberMenuGUI extends AppCompatActivity {
@@ -10,5 +9,10 @@ public class MemberMenuGUI extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.member_menu_gui_layout);
+
+        String previous = getIntent().getExtras().getString("previous");
+        if(previous.equals("CreateTeamForm")) {
+            Informer.inform("Team Created. Member Added.", findViewById(R.id.memberMenuView));
+        }
     }
 }
