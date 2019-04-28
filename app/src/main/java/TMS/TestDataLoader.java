3f55f20eb1b47ad8b2598f585ac3e3e78e6aa807
Aaron McLain <aaron.mclain@mavs.uta.edu>
2019-04-28 11:44:42 -0500

    Updated test data loader.

--- app/src/main/java/TMS/TestDataLoader.java
+++ app/src/main/java/TMS/TestDataLoader.java
@@ -1,21 +1,19 @@
 package TMS;
 
-import java.time.LocalDate;
-import java.util.Date;
-import entity.Profile;
-import entity.Project;
-import entity.Team;
-import entity.TeamFeedback;
-import entity.TeamTask;
+import entity.*;
 import gateway.ProjectStateManager;
 import model.CreateProfileRequest;
 import model.ProjectTypes.*;
+import java.time.LocalDate;
 
 public class TestDataLoader {
     public static ProjectStateManager psm = ProjectStateManager.getInstance();
     public static Project p = Project.getInstance();
     static int countTeam = 0;
     static int countProfile = 0;
+    static int countTask = 0;
+    static int countNomination = 0;
+    static int countMemberFeedback = 0;
 
     public static void loadTestData(){
         Profile manager = createManagerProfile();
@@ -24,61 +22,10 @@ public class TestDataLoader {
             psm.saveTeam(createTeam(countTeam++));
     }
 
-    public static Team createTeam(int t){
-        Team team = new Team();
-        team.teamName = "Team" + t;
-        Profile lead = createTeamLead(t, team);
-        createTeamTask(team, lead);
-        createTeamFeedback(t, team);
-        for(int m = 0; m < p.maxMembers-2; m++) {
-            Profile profile = createMemberProfile(countProfile++);
-            team.addMember(profile.email);
-            psm.saveProfile(profile);
-        }
-        return team;
-    }
-
-    private static void createTeamFeedback(int t, Team team) {
-        TeamFeedback teamFeedback = new TeamFeedback();
-        teamFeedback.date = LocalDate.now();
-        teamFeedback.teamName = team.teamName;
-        teamFeedback.feedback = "feedback" + t;
-        psm.saveTeamFeedback(teamFeedback);
-    }
-
-    private static Profile createTeamLead(int t, Team team) {
-        Profile lead = createLeadProfile(t);
-        team.addMember(lead.email);
-        psm.saveProfile(lead);
-        return lead;
-    }
-
-    private static void createTeamTask(Team team, Profile lead) {
-        TeamTask teamTask = new TeamTask();
-        teamTask.teamName = team.teamName;
-        teamTask.dueDate = LocalDate.now();
-        teamTask.description = "Description";
-        psm.saveTeamTask(teamTask);
-    }
-
     private static Profile createManagerProfile() {
         String name = "Manager0";
         Profile p = createProfile(0, name);
         p.role = Role.MANAGER;
-        return p;
-    }
-
-    public static Profile createMemberProfile(int m){
-        String name = "Member" + m;
-        Profile p = createProfile(m, name);
-        p.role = Role.MEMBER;
-        return p;
-    }
-
-    public static Profile createLeadProfile(int t){
-        String name = "Lead" + t;
-        Profile p = createProfile(t, name);
-        p.role = Role.LEAD;
         return p;
     }
 
@@ -91,5 +38,83 @@ public class TestDataLoader {
         );
         Profile p = new Profile(cpr);
         return p;
+    }
+
+    public static Team createTeam(int t){
+        Team team = new Team();
+        team.teamName = "Team" + t;
+        if(!(t == 0)) {
+            Profile lead = createTeamLead(t, team);
+            createTeamTask(team, lead);
+            createTeamFeedback(t, team);
+        }
+        for(int m = 0; m < p.maxMembers-2; m++) {
+            Profile profile = createMemberProfile(countProfile++);
+            team.addMember(profile.email);
+            psm.saveProfile(profile);
+            createMemberTask(countTask++, profile.email);
+            createNomination(countNomination++, profile.email, team.teamName);
+            createMemberFeedback(countMemberFeedback++, profile.email);
+        }
+        return team;
+    }
+
+    private static void createNomination(int n, String nominee, String teamName){
+        Nomination nomination = new Nomination(nominee, teamName, "nominator" + n + "@email.com");
+        psm.saveNomination(nomination);
+    }
+
+    private static Profile createTeamLead(int t, Team team) {
+        Profile lead = createLeadProfile(t);
+        team.addMember(lead.email);
+        team.assignTeamLead(lead.email);
+        psm.saveProfile(lead);
+        return lead;
+    }
+
+    public static Profile createLeadProfile(int t){
+        String name = "Lead" + t;
+        Profile p = createProfile(t, name);
+        p.role = Role.LEAD;
+        return p;
+    }
+
+    private static void createTeamTask(Team team, Profile lead) {
+        TeamTask teamTask = new TeamTask();
+        teamTask.teamName = team.teamName;
+        teamTask.dueDate = LocalDate.now();
+        teamTask.description = "Description";
+        psm.saveTeamTask(teamTask);
+    }
+
+    private static void createTeamFeedback(int t, Team team) {
+        TeamFeedback teamFeedback = new TeamFeedback();
+        teamFeedback.date = LocalDate.now();
+        teamFeedback.teamName = team.teamName;
+        teamFeedback.feedback = "feedback" + t;
+        psm.saveTeamFeedback(teamFeedback);
+    }
+
+    private static void createMemberFeedback(int f, String member) {
+        MemberFeedback memberFeedback = new MemberFeedback();
+        memberFeedback.date = LocalDate.now();
+        memberFeedback.memberEmail = member;
+        memberFeedback.feedback = "feedback" + f;
+        psm.saveMemberFeedback(memberFeedback);
+    }
+
+    public static Profile createMemberProfile(int m){
+        String name = "Member" + m;
+        Profile p = createProfile(m, name);
+        p.role = Role.MEMBER;
+        return p;
+    }
+
+    private static void createMemberTask(int i, String e) {
+        MemberTask memberTask = new MemberTask();
+        memberTask.memberEmail = e;
+        memberTask.dueDate = LocalDate.now();
+        memberTask.description = "Task" + i;
+        psm.saveMemberTask(memberTask);
     }
 }
