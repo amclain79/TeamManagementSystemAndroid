7e23db9a1d2de082fc5f22136eb6210688786e13
Aaron McLain <aaron.mclain@mavs.uta.edu>
2019-04-29 11:35:28 -0500

    Finished Member View Member Task interface.

--- app/src/main/java/Member/ViewMemberTaskGUI.java
+++ app/src/main/java/Member/ViewMemberTaskGUI.java
@@ -1,10 +1,15 @@
 package Member;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
+import android.widget.TextView;
 
+import TMS.Informer;
+import TMS.Main;
 import edu.uta.cse5324.team1.teammanagementsystemandroid.R;
+import entity.MemberTask;
 
 public class ViewMemberTaskGUI extends AppCompatActivity {
 
@@ -12,5 +17,18 @@ public class ViewMemberTaskGUI extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.member_view_member_task_gui_layout);
+
+        LinearLayout ll = findViewById(R.id.viewMemberTaskGUILayout);
+        LayoutParams lp = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+
+        try{
+            MemberTask memberTask = Main.viewMemberTaskController.viewMemberTask(Main.email);
+            TextView memberTaskTV = new TextView(this);
+            memberTaskTV.setText(memberTask.toString());
+            memberTaskTV.setTextSize(24.0f);
+            ll.addView(memberTaskTV, lp);
+        } catch (RuntimeException e) {
+            Informer.inform("Error Getting Task", findViewById(R.id.viewMemberTaskGUILayout));
+        }
     }
 }
